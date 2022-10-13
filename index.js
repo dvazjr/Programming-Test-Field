c6c4184a4d6655cae417c73807b069d07fd4e989
Daniel Vazquez <dvazjr@gmail.com>
2022-10-13 14:29:47 -0700

    coded password validator for character length using if/else and .trim() method

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-function sum(a, b) {
-  return a + b
-}
-function subtract(a, b) {
-  return a - b
-}
-function multiply(a, b) {
-  return a * b
-}
-function divide(a, b) {
-  return a / b
-}
-
-function run(operation, x, y) {
-  if (operation === sum) {
-    return sum(x, y)
-  }
-  if (operation === subtract) {
-    return subtract(x, y)
-  }
-  if (operation === multiply) {
-    return multiply(x, y)
-  }
-  if (operation === divide) {
-    return divide(x, y)
+function isPasswordValid(password) {
+  password = password.trim()
+  if (password.length >= 10) {
+    return true 
+  } else {
+    return false
   }
 }
 
-console.log(sum(1,3))
+console.log(isPasswordValid(" 12345678 ")) 
+//result w 10 characters including spaces is false because we trimmed the pw
+//with our code
